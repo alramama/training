e1ff161e0ac7e3d7b58e3a7c61011e74cc0a37f4
alramama <74817864+alramama@users.noreply.github.com>
2022-07-26 17:59:25 +0300

    Update training.py

--- training.py
+++ training.py
@@ -2,16 +2,33 @@ django-admin startproject CRUD
   __init__
   asgi
   settings
-  urls
-  INSTALLED_APPS = [
+      INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'appname',
-]
+    'app_name',]
+    import os
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_URL = 'static/'
+
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+  urls
+    from django.contrib import admin
+    from django.urls import path, include ,re_path
+    from django.conf import settings #added
+    from django.conf.urls.static import static ,serve
+    from django.views import *
+
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+        path('', include('App_name.urls')), #added
+
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) #added
+
 
   wsgi
   media
@@ -26,9 +43,24 @@ django-admin startproject CRUD
       Index1
     __init__
     admin
+      from .models import *
+      admin.site.register(Table_Name)
+
     apps
     forms
+      from .models import *
+
+      class CustomerForm(ModelForm):
+        class Meta:
+          model = Table_Name
+		      fields = '__all__'
+
     models
+      class Table_Name(models.Model):
+        filed_name = models.Data_Type(max_length=200, null=True)
+
     tests
     urls
+      urlpatterns = [
+	    path('page_name/', views.views_name, name="page_name"),
     views
