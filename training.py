ad3909d757af6f01dde140e15645d3f54336a1bb
alramama <74817864+alramama@users.noreply.github.com>
2022-07-26 17:50:34 +0300

    Create training.py

---
+++ training.py
@@ -0,0 +1,34 @@
+django-admin startproject CRUD
+  __init__
+  asgi
+  settings
+  urls
+  INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'appname',
+]
+
+  wsgi
+  media
+  python manage.py startapp student
+    migrations
+    templates
+      create
+      download
+      edit
+      home
+      index
+      Index1
+    __init__
+    admin
+    apps
+    forms
+    models
+    tests
+    urls
+    views
