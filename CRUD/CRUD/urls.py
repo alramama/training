60ee098add229bdce377bc1e69ff768eddda51b4
alramama <74817864+alramama@users.noreply.github.com>
2022-07-28 15:36:16 +0300

    Update urls.py

--- CRUD/CRUD/urls.py
+++ CRUD/CRUD/urls.py
@@ -21,6 +21,6 @@ from django.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('student.urls')), #added
+    path('', include('BID.urls')), #added
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) #added
