d06d0415fb5fe6a858fd4599eb3e19b8f8a09767
alramama <74817864+alramama@users.noreply.github.com>
2022-07-28 15:45:55 +0300

    Update urls.py

--- CRUD/student/urls.py
+++ CRUD/student/urls.py
@@ -2,12 +2,13 @@ from student import views
 #from django.conf.urls import include, url
 from django.urls import path
 
-urlpatterns = [
+urlpatterns = [path('', views.show_stud),]
+
+"""
     path('create_stud', views.create_stud),
-    path('', views.show_stud),
     path('show_stud', views.show_stud),
     path('update_stud/<str:id>', views.update_stud),
     path('delete_stud/<str:id>', views.delete_stud),
     path("index1", views.index1, name="index1")
 
-]
+"""
