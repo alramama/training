4b794f03ba20486f5b44952064d677173a5aabd9
alramama <74817864+alramama@users.noreply.github.com>
2022-08-05 20:52:41 +0300

    Update models.py

--- project/films/models.py
+++ project/films/models.py
@@ -4,34 +4,13 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
-class Genre(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
 
 class Film(models.Model):
-    title = models.CharField(max_length=200, )
-    length = models.PositiveIntegerField(blank=True, null=True)
-    year = models.PositiveIntegerField(blank=True, null=True)
-    files = models.FileField(blank=True, null=True,upload_to='media')
-    score = models.FloatField(blank=True, null=True, validators=[MinValueValidator(0), MaxValueValidator(10)])
-    genre = models.ForeignKey(Genre, blank=True, null=True, on_delete=models.CASCADE)
+    bid_description  = models.CharField(blank = True, max_length =255 )
+    End_user = models.CharField(blank = True, max_length =255 )
+    activity_type = models.CharField(blank = True, max_length =255 )
+    reference_no = models.CharField(blank = True, max_length =50 )
+    last_date_of_clarifications = models.DateField()
+    bid_file = models.FileField(upload_to='media',blank = True)
+    
 
-    def __str__(self):
-        if self.year:
-            return f"{self.title} ({self.year})"
-        return self.title
-
-    def get_fields(self):
-        return [(field.verbose_name, field.value_from_object(self))
-
-                if field.verbose_name != 'genre'
-
-                else
-                (field.verbose_name,
-                 Genre.objects.get(pk=field.value_from_object(self)).name)
-
-                for field in self.__class__._meta.fields[1:]
-                ]
